1323a6e5247f56ffe535bb75872676fb8846ac5c
mariemmarrekchi <mariem2marrekchi@gmail.com>
2023-09-22 14:04:03 +0100

    first commit devops premiére seance

---
+++ src/test/java/tn/enis/demo/Demo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
